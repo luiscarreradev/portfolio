061f6198056dbe2adabc4a14ec0b6a5d78aa8d82
Luis Carrera <luiscarreramv@gmail.com>
2022-02-20 20:32:10 -0600

    Created Header adn Home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
 /*===== SHOW MENU =====*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.querySelector(toggleId)
-    const nav = document.querySelector(navId)
+    const navList = document.querySelector(navId)
 
-    if(toggle && nav){
+    if(toggle && navList){
         toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
+            navList.classList.toggle('show')
         })
     }
 }
 
-showMenu('.nav__icon--menu','.nav__list');
+showMenu('.nav__toggle','.nav__list');
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link')
+console.log(navLink)
+
+function linkAction(){
+    // Active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    // Remove menu mobile
+    const navMenu = document.querySelector('.nav__list')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
